ebe9b9a25e81bbdf6e13b456d442a7f7554320b0
Jason Lefever <jlefever@ycp.edu>
2015-05-04 22:36:52 -0400

    keyboard input

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,21 @@ extern crate glutin_window;
 extern crate opengl_graphics;
 
 use piston::window::WindowSettings;
+use piston::input::{ Button, Key };
 use piston::event::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
-// enum Direction {
-// 	Up,
-// 	Down,
-// 	Right,
-// 	Left
-// }
+enum Direction {
+	Up,
+	Down,
+	Right,
+	Left
+}
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    // dir: Direction,   // Direction for the square.
+    dir: Direction,   // Direction for the square.
     speed: f64,
     x: f64,
     y: f64
@@ -30,9 +31,7 @@ impl App {
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        // let dir = self.dir;
-        let speed = self.speed;
+        let square = rectangle::square(0.0, 0.0, 10.0);
         let x = self.x;
         let y = self.y;
         //let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
@@ -49,8 +48,12 @@ impl App {
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-    	println!("{}", args.dt);
-        self.x += self.speed * args.dt;
+        match self.dir {
+        	Direction::Up    => self.y -= self.speed * args.dt,
+        	Direction::Down  => self.y += self.speed * args.dt,
+        	Direction::Right => self.x += self.speed * args.dt,
+        	Direction::Left  => self.x -= self.speed * args.dt
+        }
     }
 }
 
@@ -62,7 +65,7 @@ fn main() {
         opengl,
         WindowSettings::new(
             "game",
-            [800, 800]
+            [500, 500]
         )
         .exit_on_esc(true)
     );
@@ -70,6 +73,7 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
+        dir: Direction::Right,
         speed: 250.0,
         x: 100.0,
         y: 100.0
@@ -82,6 +86,16 @@ fn main() {
 
         if let Some(u) = e.update_args() {
             app.update(&u);
+        }
+
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+        	match key {
+        		Key::W => app.dir = Direction::Up,
+        		Key::A => app.dir = Direction::Left,
+        		Key::S => app.dir = Direction::Down,
+        		Key::D => app.dir = Direction::Right,
+        		_ => ()
+        	}
         }
     }
 }
