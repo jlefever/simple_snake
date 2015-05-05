44ec36879cc72b3e7c3bf994c7697b2249b19bc9
Jason Lefever <jlefever@ycp.edu>
2015-05-04 22:05:07 -0400

    it moves!

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,19 @@ use piston::event::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
+// enum Direction {
+// 	Up,
+// 	Down,
+// 	Right,
+// 	Left
+// }
+
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64   // Rotation for the square.
+    // dir: Direction,   // Direction for the square.
+    speed: f64,
+    x: f64,
+    y: f64
 }
 
 impl App {
@@ -21,16 +31,17 @@ impl App {
         const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
         let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
+        // let dir = self.dir;
+        let speed = self.speed;
+        let x = self.x;
+        let y = self.y;
+        //let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(GREEN, gl);
 
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
+            let transform = c.transform.trans(x, y);
 
             // Draw a box rotating around the middle of the screen.
             rectangle(RED, square, transform, gl);
@@ -38,8 +49,8 @@ impl App {
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
+    	println!("{}", args.dt);
+        self.x += self.speed * args.dt;
     }
 }
 
@@ -50,8 +61,8 @@ fn main() {
     let window = Window::new(
         opengl,
         WindowSettings::new(
-            "spinning-square",
-            [200, 200]
+            "game",
+            [800, 800]
         )
         .exit_on_esc(true)
     );
@@ -59,7 +70,9 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0
+        speed: 250.0,
+        x: 100.0,
+        y: 100.0
     };
 
     for e in window.events() {
