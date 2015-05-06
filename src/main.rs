94708a22330354aa271132f6e6fb17725d01d09d
Jason Lefever <jlefever@ycp.edu>
2015-05-06 02:04:53 -0400

    actually a snake game instead of a moving square

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+ #![feature(slice_patterns)]
 extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
 extern crate opengl_graphics;
+
+use std::collections::LinkedList;
 
 use piston::window::WindowSettings;
 use piston::input::{ Button, Key };
 use piston::event::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
+
+type Location = [i32; 2];
 
 enum Direction {
     Up,
@@ -16,43 +21,53 @@ enum Direction {
     Left
 }
 
+struct Model {
+    snake: LinkedList<Location>,
+    dir: Direction
+}
+
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    dir: Direction, // Direction for the square.
-    speed: f64,
-    x: f64,
-    y: f64
+    model: Model
+}
+
+impl Model {
+    fn tick(&mut self) {
+        let &[row, col] = self.snake.front().unwrap();
+        match self.dir {
+            Direction::Up    => self.snake.push_front([row, col - 1]),
+            Direction::Down  => self.snake.push_front([row, col + 1]),
+            Direction::Right => self.snake.push_front([row + 1, col]),
+            Direction::Left  => self.snake.push_front([row - 1, col])
+        }
+        self.snake.pop_back();
+    }
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const BLACK:   [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-        let mut square = [(args.width / 2) as f64, (args.height / 2) as f64, 10.0, 10.0];
-        let (x, y) = (self.x, self.y);
+        const SIZE: f64 = 15.0;
+
+        let ref snake = self.model.snake;
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
-            clear(GREEN, gl);
+            clear(WHITE, gl);
 
-            square[0] = x;
-            square[1] = y;
+            for loc in snake {
+                rectangle(BLACK, [loc[0] as f64 * SIZE, loc[1] as f64 * SIZE, SIZE, SIZE], c.transform, gl);
+            }
 
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, c.transform, gl);
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        match self.dir {
-            Direction::Up    => self.y -= self.speed * args.dt,
-            Direction::Down  => self.y += self.speed * args.dt,
-            Direction::Right => self.x += self.speed * args.dt,
-            Direction::Left  => self.x -= self.speed * args.dt
-        }
+        // TODO
     }
 }
 
@@ -63,19 +78,26 @@ fn main() {
     let window = Window::new(
         opengl,
         WindowSettings::new(
-            "game",
+            "Simple Snake",
             [500, 500]
         )
         .exit_on_esc(true)
     );
 
-    // Create a new game and run it.
+    let mut snake = LinkedList::new();
+    snake.push_front([5, 7]);
+    snake.push_front([5, 8]);
+    snake.push_front([5, 9]);
+    snake.push_front([6, 9]);
+
+    let model = Model {
+        snake: snake,
+        dir: Direction::Left
+    };
+
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        dir: Direction::Right,
-        speed: 250.0,
-        x: 100.0,
-        y: 100.0
+        model: model
     };
 
     for e in window.events() {
@@ -89,11 +111,11 @@ fn main() {
 
         if let Some(Button::Keyboard(key)) = e.press_args() {
             match key {
-                Key::W => app.dir = Direction::Up,
-                Key::A => app.dir = Direction::Left,
-                Key::S => app.dir = Direction::Down,
-                Key::D => app.dir = Direction::Right,
-                _ => ()
+                Key::W => app.model.dir = Direction::Up,
+                Key::A => app.model.dir = Direction::Left,
+                Key::S => app.model.dir = Direction::Down,
+                Key::D => app.model.dir = Direction::Right,
+                _ => app.model.tick()
             }
         }
     }
